a4a5a36b123d3a1383fafcd25e22fa4933567829
Robson Mendes <robsonmendes_1@outlook.com>
2025-09-13 23:07:46 -0300

    login layout pronto

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Login } from './pages/admin/login/login';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    component: Login
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
